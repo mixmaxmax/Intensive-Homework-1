391093704c3d2e15b8f24ecb181a9d82b0e66397
mixmaxmax <max.mikheenko2003@gmail.com>
2026-07-04 02:29:31 +0300

    Временный(?) отказ от имплементации интерфейса, определены основные поля класса CustomHashMap и вложенного класа Node, добавлен метод put, переопределен toString, обновлен .gitignore

--- src/CustomHashMap.java
+++ src/CustomHashMap.java
@@ -1,16 +1,77 @@
-public class CustomHashMap implements  RequiredMethods{
-    @Override
-    public void get() {
+/**Кастомная хэш-мапа*/
+public class CustomHashMap<K, V> {
 
+    private Node<K, V>[] buckets; //массив корзин
+    private int size; //текущий счетчик элементов
+    private int capacity; //максимальная вместимость мапы
+    public CustomHashMap() {
+        capacity = 16; //фиксированный размер массива
+        buckets = new Node[capacity];
+        size = 0;
     }
 
-    @Override
-    public void put() {
-
+    /**Вычисление ячейки массива по остатку от деления*/
+    private int calcIndex(K key) {
+        return Math.abs(key.hashCode()) % capacity;
     }
 
-    @Override
-    public void remove() {
+    /**Добавление в мапу*/
+    public void put(K key, V value) {
+        try {
+            if (key == null) {
+                throw new IllegalArgumentException("Передаваемый ключ не может быть null");
+            }
 
+            int index = calcIndex(key); //вычисление индекса
+
+            Node<K, V> current = buckets[index];
+            while (current!=null) { //поиск узла
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                } else {
+                    current = current.next;
+                }
+            }
+
+            Node<K, V> newNode = new Node<>(key, value, buckets[index]);
+            buckets[index] = newNode;
+            size++;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+     @Override
+     public String toString() {
+        StringBuilder sb = new StringBuilder("");
+        if (size == 0) {
+            return "CustomHashMap is empty: {}";
+        }
+        else {
+            for (int i = 0; i < capacity; i++) {
+                Node<K, V> current = buckets[i];
+                while (current!=null) {
+                    sb.append("{" + current.key + " = " + current.value + "} ");
+                    current = current.next;
+                }
+            }
+        }
+        return sb.toString();
+     }
+
+
+     /**Вложенный класс. Узел связанного списка*/
+    private static class Node<K,V> {
+        private K key; //ключ
+        private V value; //значение
+        private Node<K, V> next; //ссылка на следующий узел
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ import java.util.HashMap;
 
 public class Main {
     public static void main (String[] args) {
-
+        CustomHashMap<String, Integer> numbersOfPlayers = new CustomHashMap<>();
+        numbersOfPlayers.put("Cristiano", 7);
+        numbersOfPlayers.put("Messi", 10);
+        numbersOfPlayers.put("Mbappe", 10);
+        numbersOfPlayers.put("Zidane", 5);
+        IO.println(numbersOfPlayers);
     }
 }
